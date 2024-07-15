dcd9e534d22a6b3cb5af2173021156da2742df9c
Yadi <yadi@millennia-solusi.id>
2024-07-16 06:18:59 +0700

    new porfolio

---
+++ components/PortfolioItem.tsx
@@ -0,0 +1,84 @@
+import { PropsWithChildren, useMemo } from "react"
+import type { Icons } from "./component-icon"
+import { BootstrapIcon, CSSIcon, ExpressIcon, GithubIcon, GolangIcon, HTMLIcon, JavascriptIcon, LaravelIcon, LUAIcon, NextJsIcon, NuxtIcon, PostgresqlIcon, ReactIcon, ReactNativeIcon, RobloxStudioIcon, SocketIOIcon, VueIcon } from "./component-icon"
+
+
+
+export interface PortfolioItemProps extends PropsWithChildren {
+  img: string,
+  altImg: string,
+  project: {
+    title: string,
+    link: string,
+  },
+  techStacks?: Array<Icons>
+}
+
+export default function PortfolioItem(props: PortfolioItemProps) {
+
+  const techStackItem = useMemo(() => {
+    if (Array.isArray(props.techStacks) && props.techStacks.length) {
+      return props.techStacks.map((techStack, i) => {
+        switch (techStack) {
+          case 'react':
+            return <ReactIcon />
+          case 'javascript':
+            return <JavascriptIcon />
+          case 'postgresql':
+            return <PostgresqlIcon />
+          case 'html':
+            return <HTMLIcon />
+          case 'css':
+            return <CSSIcon />
+          case 'socketIO':
+            return <SocketIOIcon />
+          case 'express':
+            return <ExpressIcon />
+          case 'reactNative':
+            return <ReactNativeIcon />
+          case 'bootstrap':
+            return <BootstrapIcon />
+          case 'github':
+            return <GithubIcon />
+          case 'golang':
+            return <GolangIcon />
+          case 'laravel':
+            return <LaravelIcon />
+          case 'vue':
+            return <VueIcon />
+          case 'nuxt':
+            return <NuxtIcon />
+          case 'lua':
+            return <LUAIcon />
+          case 'robloxStudio':
+            return <RobloxStudioIcon />
+          case 'nextjs':
+            return <NextJsIcon />
+        }
+      })
+    } else {
+      return null
+    }
+  }, [props.techStacks])
+
+  return (
+    <div className="mb-12 p-4 md:w-1/2">
+      <div className="rounded-md shadow-md overflow-hidden">
+        <img src={props.img} alt={props.altImg} className="w-full" />
+      </div>
+      <h3 className="font-semibold text-xl text-dark mt-5 mb-3"><a href={props.project.link} target="_blank" rel="noreferrer" className="thickness-link">{props.project.title}</a></h3>
+      <p className="font-medium text-base text-secondary text-justify">{props.children}</p>
+      {
+        Array.isArray(props.techStacks) && props.techStacks.length
+          ?
+          <div className="mt-5">
+            <h2 className="mb-3">Tech Stacks:</h2>
+            <div className="flex items-center gap-3 max-w-sm">
+              {techStackItem}
+            </div>
+          </div>
+          : null
+      }
+    </div>
+  )
+}
--- components/component-home/PortfolioSection.tsx
+++ components/component-home/PortfolioSection.tsx
@@ -1,4 +1,130 @@
+import PortfolioItem, { PortfolioItemProps } from "../PortfolioItem"
+
 export default function PortfolioSection() {
+  const portfolio: Array<PortfolioItemProps> = [
+    {
+      img: "/assets/images/portfolio/waysbeans.png",
+      altImg: "Waysbeans Capture",
+      project: {
+        title: "Waysbeans",
+        link: "https://waysbeans-by-yadi.netlify.app/"
+      },
+      children: <>
+        This is my task result from <a href="https://dumbways.id/" target="_blank" rel="noreferrer" className="text-blue-600">Dumbways Id</a>. - E-Commerce Web App
+      </>,
+      techStacks: ['javascript', 'react', 'express', 'socketIO'],
+    },
+    {
+      img: "/assets/images/portfolio/npm.png",
+      altImg: "NPM Exposql-Queries Capture",
+      project: {
+        title: "expostsql-queries",
+        link: "https://www.npmjs.com/package/expostsql-queries"
+      },
+      children: <>
+        My First Package Manager Project, this is a package module for querying the postgresql database in Framework EXPRESSjs such a eloquent orm in Laravel.
+      </>,
+      techStacks: ['javascript', 'postgresql'],
+    },
+    {
+      img: "/assets/images/portfolio/jewel.jpg",
+      altImg: "Jewel Capture",
+      project: {
+        title: "Jewel - Landing Page",
+        link: "https://codepen.io/yadbro/full/ExQarJx"
+      },
+      children: <>
+        This is a landing page that I made without using any framework, and is also a challenge from Responsive Web Design FreeCodeCamp.
+        However, the challenge looks different because something has to be added to complete the test.
+      </>,
+      techStacks: ['html', 'css', 'javascript'],
+    },
+    {
+      img: "/assets/images/portfolio/mobile-app.png",
+      altImg: "Mobile App Dumbways Capture",
+      project: {
+        title: "Dumbways (Mobile App)",
+        link: "https://drive.google.com/file/d/1RiIW9FKMNFHVo1swDvKwzgGLedmkT4YB/view?usp=sharing"
+      },
+      children: <>
+        This is my task result from <a href="https://dumbways.id/" target="_blank" rel="noreferrer" className="text-blue-600">Dumbways Id</a>. - Mobile App
+      </>,
+      techStacks: ['javascript', 'reactNative'],
+    },
+    {
+      img: "/assets/images/portfolio/css-layout.png",
+      altImg: "Technical Documentation Capture",
+      project: {
+        title: "Technical Document Page",
+        link: "https://codepen.io/yadbro/full/NWyPyMY"
+      },
+      children: <>
+        This is a Technical document page that I made without using any framework, and is also a challenge from Responsive Web Design FreeCodeCamp.
+      </>,
+      techStacks: ['html', 'css', 'javascript',],
+    },
+    {
+      img: "/assets/images/portfolio/logo-jiep.png",
+      altImg: "JIEP Capture",
+      project: {
+        title: "JIEP",
+        link: "https://jiep.co.id/"
+      },
+      children: <>
+        This is main website from JIEP, we remake all each element of JIEP with fresh or modern website.
+      </>,
+      techStacks: ['javascript', 'vue', 'nuxt'],
+    },
+    {
+      img: "/assets/images/portfolio/logo-jiep.png",
+      altImg: "PPID JIEP Capture",
+      project: {
+        title: "PPID JIEP",
+        link: "https://ppid.jiep.co.id/"
+      },
+      children: <>
+        This is ppid website from JIEP, we remake all each element of PPID JIEP with fresh or modern website.
+      </>,
+      techStacks: ['javascript', 'vue', 'nuxt'],
+    },
+    {
+      img: "/assets/images/portfolio/logo-jiep.png",
+      altImg: "Jiep Record Center Admin Capture",
+      project: {
+        title: "JIEP Record Center",
+        link: "https://recordcenter.jiep.co.id"
+      },
+      children: <>
+        Helps create features (Pemusnahan, Otorisasi), debungging, etc.
+      </>,
+      techStacks: ['javascript', 'vue'],
+    },
+    {
+      img: "/assets/images/portfolio/CJS-Team.png",
+      altImg: "Creativity Jannah Studio Capture",
+      project: {
+        title: "Creativity Jannah Studio",
+        link: "https://creativity-jannah-studio.vercel.app/"
+      },
+      children: <>
+        This is my studio community with the aim of combining technology, art, and gaming to create something beautiful <strong>(jannah)</strong>.
+      </>,
+      techStacks: ['javascript', 'react', 'nextjs', 'lua', 'robloxStudio'],
+    },
+    {
+      img: "/assets/images/portfolio/beaconcream-escape-2.webp",
+      altImg: "Beaconcream Escape 2 Capture",
+      project: {
+        title: "Beaconcream Escape 2",
+        link: "https://www.roblox.com/id/games/8246593290/Beaconcream-Escape-2"
+      },
+      children: <>
+        Making websites every day is boring. I needed something new, so I tried other development, and ended up making a Roblox obby story game.
+      </>,
+      techStacks: ['lua', 'robloxStudio'],
+    },
+  ]
+
   return (
     <>
       <section id="portfolio" className="pt-32 pb-16 bg-slate-100">
@@ -13,56 +139,19 @@ export default function PortfolioSection() {
           </div>
 
           <div className="w-full px-4 flex flex-wrap justify-center xl:w-10/12 xl:mx-auto">
-            
-            <div className="mb-12 p-4 md:w-1/2">
-              <div className="rounded-md shadow-md overflow-hidden">
-                <img src="/assets/images/portfolio/waysbeans.png" alt="E-Commerce" className="w-full" />
-              </div>
-              <h3 className="font-semibold text-xl text-dark mt-5 mb-3"><a href="https://waysbeans-by-yadi.netlify.app/" target="_blank" rel="noreferrer" className="thickness-link">WaysBeans</a></h3>
-              <p className="font-medium text-base text-secondary text-justify">This is my task result from <a href="https://dumbways.id/" target="_blank" rel="noreferrer" className="text-blue-600">Dumbways Id</a>. - E-Commerce Web App</p>
-            </div>
-            
-            <div className="mb-12 p-4 md:w-1/2">
-              <div className="rounded-md shadow-md overflow-hidden">
-                <img src="/assets/images/portfolio/npm.png" alt="npm" className="w-full" />
-              </div>
-              <h3 className="font-semibold text-xl text-dark mt-5 mb-3"><a href="https://www.npmjs.com/package/expostsql-queries" target="_blank" rel="noreferrer" className="thickness-link">expostsql-queries</a></h3>
-              <p className="font-medium text-base text-secondary text-justify">My First Package Manager Project, this is a package module for querying the postgresql database in Framework EXPRESSjs such a eloquent orm in Laravel.</p>
-            </div>
-
-            <div className="mb-12 p-4 md:w-1/2">
-              <div className="rounded-md shadow-md overflow-hidden">
-                <img src="/assets/images/portfolio/jewel.jpg" alt="Landing Page" className="w-full" />
-              </div>
-              <h3 className="font-semibold text-xl text-dark mt-5 mb-3"><a href="https://codepen.io/yadbro/full/ExQarJx" target="_blank" rel="noreferrer" className="thickness-link">Jewel - Landing Page</a></h3>
-              <p className="font-medium text-base text-secondary text-justify">This is a landing page that I made without using any framework, and is also a challenge from Responsive Web Design FreeCodeCamp.
-However, the challenge looks different because something has to be added to complete the test.</p>
-            </div>
-
-            <div className="mb-12 p-4 md:w-1/2">
-              <div className="rounded-md shadow-md overflow-hidden">
-                <img src="/assets/images/portfolio/mobile-app.png" alt="Mobile App Image" className="w-full" />
-              </div>
-              <h3 className="font-semibold text-xl text-dark mt-5 mb-3"><a href="https://drive.google.com/file/d/1RiIW9FKMNFHVo1swDvKwzgGLedmkT4YB/view?usp=sharing" target="_blank" rel="noreferrer" className="thickness-link">Dumbways (Mobile App)</a></h3>
-              <p className="font-medium text-base text-secondary text-justify">This is my task result from <a href="https://dumbways.id/" target="_blank" rel="noreferrer" className="text-blue-600">Dumbways Id</a>. - Mobile App</p>
-            </div>
-
-            <div className="mb-12 p-4 md:w-1/2">
-              <div className="rounded-md shadow-md overflow-hidden">
-                <img src="/assets/images/portfolio/css-layout.png" alt="Technical Document Page Image" className="w-full" />
-              </div>
-              <h3 className="font-semibold text-xl text-dark mt-5 mb-3"><a href="https://codepen.io/yadbro/full/NWyPyMY" target="_blank" rel="noreferrer" className="thickness-link">Technical Document Page</a></h3>
-              <p className="font-medium text-base text-secondary text-justify">This is a Technical document page that I made without using any framework, and is also a challenge from Responsive Web Design FreeCodeCamp.</p>
-            </div>
-
-            <div className="mb-12 p-4 md:w-1/2">
-              <div className="rounded-md shadow-md overflow-hidden">
-                <img src="/assets/images/portfolio/logo-jiep.png" alt="Logo Jiep" className="w-full" />
-              </div>
-              <h3 className="font-semibold text-xl text-dark mt-5 mb-3"><a href="https://recordcenter.jiep.co.id" target="_blank" rel="noreferrer" className="thickness-link">JIEP Record Center</a></h3>
-              <p className="font-medium text-base text-secondary text-justify">Helps create features (Pemusnahan, Otorisasi), debungging, etc.</p>
-            </div>
-
+            {
+              portfolio.map((p, i) => (
+                <PortfolioItem
+                  key={`porfolio-item-${i}`}
+                  img={p.img}
+                  project={p.project}
+                  altImg={p.altImg}
+                  techStacks={p.techStacks}
+                >
+                  {p.children}
+                </PortfolioItem>
+              ))
+            }
           </div>
           <div className="flex justify-center thickness-link">
             <a href="https://www.linkedin.com/in/yadi-apriyadi/details/featured/" target="_blank" rel="noreferrer" className="text-blue-600">More...</a>
